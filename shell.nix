64fbf996d8c6d792e7fcb22ec1d665c7377d2c5b
phanirithvij <phanirithvij2000@gmail.com>
2024-10-01 16:34:32 +0530

    initial noogle, nix manuals scripts
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,30 +1,57 @@
 let
-  # TODO lot
   sources = import ./npins;
   pkgs = import sources.nixpkgs { };
-  nix = import sources.nix { };
+  inherit (pkgs) lib;
+b' = lib.meta.getExe;
+  noogle = rec {
+    src = sources.noogle;
+    # offline instance of migrate-doc-comments ?? am I crazy?
+    # https://github.com/hsjobeki/nixpkgs/blob/master/.github/workflows/update-comments.yml
+    build-script = pkgs.writeShellScriptBin "noogle-build" ''
+      ${nix} build ${sources.noogle}#ui -o result/noogle
+    '';
+    run-script = pkgs.writeShellScriptBin "noogle-run" ''
+      ${b' build-script}
+      ${b' pkgs.httplz} -q -x ./result/noogle/lib/node_modules/noogle/out
+    '';
+  };
   mmdocs = { }; # todo get that github pr?
-  noogle = { }; # figure out pagefind
   multipageofficial = { }; # https://github.com/NixOS/nixpkgs/compare/nixos-24.05...GetPsyched:nixpkgs:render-docs?w=1
   nixpkgs-tracker = { }; # nixpkgs tracker
   nixpkgs-pr-tracker = { }; # pr-tracker
-  qutebrowser = { }; # w3m alternative
-  w3m = pkgs.w3m;
-  browser = w3m;
-  nix-docs = pkgs.writeShellScript "nix-docs" ''
-    w3m $(nix build github:nixos/nixpkgs/nixos-unstable#nixpkgs-manual --print-out-paths)/share/doc/nixpkgs/manual.html
-    w3m $(nix-build nixos/release.nix -A manualHTML.x86_64-linux)/share/doc/nixos/index.html
-    w3m $(nix build github:nixos/nix#nix^doc --print-out-paths)/share/doc/nix/manual/index.html
+  nix = ''${b' pkgs.nix} --extra-experimental-features "nix-command flakes"'';
+  # TODO tmuxp for w3m
+  docs-browse =
+    browser:
+    pkgs.writeShellScriptBin "docs-browse" ''
+      ${b' docs-build}
+      ${browser} ./result/nix-latest-doc/share/doc/nix/manual/index.html
+      ${browser} ./result/nixpkgs-git-manual/share/doc/nixpkgs/manual.html
+      ${browser} ./result/nixos-git-manual/share/doc/nixos/index.html
+    '';
+  docs-build = pkgs.writeShellScriptBin "docs-build" ''
+    ${nix} build ${sources.nix}#nix^doc --print-out-paths -o result/nix-latest-doc
+    ${nix} build ${sources.nixpkgs}#nixpkgs-manual --print-out-paths -o result/nixpkgs-git-manual
+    ${pkgs.nix}/bin/nix-build ${sources.nixpkgs}/nixos/release.nix -A manualHTML.x86_64-linux -o result/nixos-git-manual
   '';
+  scripts = [
+    docs-build
+    (docs-browse (b' pkgs.w3m)) # pkgs.qutebrowser
+    noogle.build-script
+    noogle.run-script
+  ];
 in
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    black
-    (python312.withPackages (pypkgs: with pypkgs; [ beautifulsoup4 ]))
+  packages =
+    with pkgs;
+    [
+      black
+      (python312.withPackages (pypkgs: with pypkgs; [ beautifulsoup4 ]))
 
-    npins
-    nixfmt-rfc-style
-    deadnix
-    statix
-  ];
+      npins
+      nixfmt-rfc-style
+      deadnix
+      statix
+    ]
+    ++ scripts;
 }
